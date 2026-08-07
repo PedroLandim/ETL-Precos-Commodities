6f706e85b69d54d90d483e55256734978dccea94
PedroLandim <plb3@cin.ufpe.br>
2026-08-07 18:17:36 -0300

    adicionado feature de extração

---
+++ src/extract-load.py
@@ -0,0 +1,48 @@
+# imports
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+
+# Variáveis de ambiente
+commodities = ['GC=F', 'CL=F', 'NG=F', 'SI=F']
+
+load_dotenv()
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_SCHEMA = os.getenv('DB_SCHEMA')
+
+DB_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DB_URL)
+
+# extração de dados
+
+def buscar_dados_commodities(simbolo, period="1y"):
+    ticker = yf.Ticker(simbolo)
+    dados = ticker.history(period=period)[["Close"]]
+    dados["simbolo"] = simbolo
+    return dados
+
+def buscar_todos_dados_commodities(commodities):
+    todos_dados = []
+    for simbolo in commodities:
+        dados = buscar_dados_commodities(simbolo)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
+
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, schema=schema, if_exists='replace', index=True, index_label='date')
+
+# inicialização teste
+
+if __name__ == "__main__":
+    dados_concatenados = buscar_todos_dados_commodities(commodities)
+    salvar_no_postgres(dados_concatenados)
